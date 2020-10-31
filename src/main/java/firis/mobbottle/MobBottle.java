9c0664e5b3cab1034b2b9f4e72a17788331cedd8
firis <firis.games.lapismoonlight@gmail.com>
2020-10-31 18:21:39 +0900

    ver1.0.0

--- src/main/java/firis/mobbottle/MobBottle.java
+++ src/main/java/firis/mobbottle/MobBottle.java
@@ -51,7 +51,7 @@ public class MobBottle
 {
     public static final String MODID = "mobbottle";
     public static final String NAME = "Mob Bottle";
-    public static final String VERSION = "0.6";
+    public static final String VERSION = "1.0.0";
     public static final String MOD_DEPENDENCIES = "required-after:forge@[1.12.2-14.23.5.2768,);"
     		+ "after:jei@[1.12.2-4.13.1.220,);"
     		+ "after:lmreengaged@[9.3.0,)";

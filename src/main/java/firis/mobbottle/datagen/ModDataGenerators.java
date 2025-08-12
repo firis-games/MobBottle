0a1c01ab22e2703049a6f82fe21d75204eb75ce4
firis <firis.games.lapismoonlight@gmail.com>
2025-08-12 22:07:58 +0900

    レシピ用データジェネレーター追加
    レシピの登録を手動からデータジェネレーターへ変更
    手動レシピの削除

---
+++ src/main/java/firis/mobbottle/datagen/ModDataGenerators.java
@@ -0,0 +1,18 @@
+package firis.mobbottle.datagen;
+
+import firis.mobbottle.MobBottle;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+@EventBusSubscriber(modid = MobBottle.MODID)
+public class ModDataGenerators {
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent.Client event) {
+
+        //レシピの登録
+        event.createProvider(ModRecipeProvider.Runner::new);
+
+    }
+}
---
+++ src/main/java/firis/mobbottle/datagen/ModRecipeProvider.java
@@ -0,0 +1,78 @@
+package firis.mobbottle.datagen;
+
+import firis.mobbottle.MobBottle;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.*;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends RecipeProvider {
+
+
+    private static final TagKey<Item> TAG_GLASS_CHEAP = TagKey.create(
+            Registries.ITEM, ResourceLocation.fromNamespaceAndPath("c", "glass_blocks/cheap"));
+
+    private static final TagKey<Item> TAG_GEM = TagKey.create(
+            Registries.ITEM, ResourceLocation.fromNamespaceAndPath(MobBottle.MODID, "gems"));
+
+
+    public ModRecipeProvider(HolderLookup.Provider registries, RecipeOutput output) {
+        super(registries, output);
+    }
+
+    @Override
+    protected void buildRecipes() {
+
+        //レシピ登録
+        //モブボトル
+        ShapedRecipeBuilder
+                .shaped(this.registries.lookupOrThrow(Registries.ITEM), RecipeCategory.MISC, MobBottle.FirisItems.MOB_BOTTLE.get(), 1)
+                .pattern("www")
+                .pattern("gdg")
+                .pattern("ggg")
+                .define('g', TAG_GLASS_CHEAP)
+                .define('w', ItemTags.WOODEN_SLABS)
+                .define('d', TAG_GEM)
+                .unlockedBy("has_gem", this.has(TAG_GEM))
+                .save(this.output);
+
+        //モブボトル -> 空のモブボトル
+        ShapelessRecipeBuilder
+                .shapeless(this.registries.lookupOrThrow(Registries.ITEM), RecipeCategory.MISC, MobBottle.FirisItems.MOB_BOTTLE_EMPTY.get(), 1)
+                .requires(MobBottle.FirisItems.MOB_BOTTLE.get())
+                .unlockedBy("has_gem", this.has(TAG_GEM))
+                .save(this.output, MobBottle.MODID + ":mob_bottle_1");
+
+        //空のモブボトル -> モブボトル
+        ShapelessRecipeBuilder
+                .shapeless(this.registries.lookupOrThrow(Registries.ITEM), RecipeCategory.MISC, MobBottle.FirisItems.MOB_BOTTLE.get(), 1)
+                .requires(MobBottle.FirisItems.MOB_BOTTLE_EMPTY.get())
+                .unlockedBy("has_gem", this.has(TAG_GEM))
+                .save(this.output, MobBottle.MODID + ":mob_bottle_2");
+    }
+
+    public static class Runner extends RecipeProvider.Runner {
+
+        public Runner(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider) {
+            super(output, lookupProvider);
+        }
+
+        @Override
+        protected RecipeProvider createRecipeProvider(HolderLookup.Provider provider, RecipeOutput output) {
+            return new ModRecipeProvider(provider, output);
+        }
+
+        @Override
+        public String getName() {
+            return "ModRecipeProvider";
+        }
+    }
+}
